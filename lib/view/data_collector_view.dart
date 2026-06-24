47683b655e8b52d10b1adb92eb562927803cdc19
Nguyễn Hoàng Minh <nhoangminh131@gmail.com>
2026-06-25 00:56:02 +0700

    feat(DataCollectorView): add basic page for data collection step

---
+++ lib/view/data_collector_view.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class DataCollectorView extends StatelessWidget {
+  const DataCollectorView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Data Collector'),
+        centerTitle: false,
+        scrolledUnderElevation: 0,
+        backgroundColor: Theme.of(context).colorScheme.surface, 
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 60),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Collection Phase',
+                style: Theme.of(context).textTheme.headlineLarge,
+              ),
+              Spacer(),
+              Container(
+                height: 320,
+                width: 270,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.surface,
+                  border: Border.all(
+                    color: Theme.of(context).colorScheme.outline,
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  child: ListView(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    children: [
+                      for (int i = 0; i < 40; i++)
+                        const Text('Relatively long text stream'),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

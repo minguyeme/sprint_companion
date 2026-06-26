5ed57e4724fd63af74a0e2a51953790a15d285b0
Nguyễn Hoàng Minh <nhoangminh131@gmail.com>
2026-06-26 19:21:52 +0700

    wip(SensorService): add the aggregrate sensor data model a wip SensorService

--- lib/core/aggregate_sensor_data.dart
+++ lib/core/aggregate_sensor_data.dart
@@ -1,1 +1,38 @@
+typedef Vector3D = ({double x, double y, double z});
+typedef GpsData = ({
+  double longitude,
+  double latitude,
+  double altitude,
+  double accuracy,
+  double speed,
+  double heading,
+});
 
+/// Data model containing a snapshot of GPS, accelerometer and gyroscope sensor data.
+class AggregateSensorData {
+  final DateTime masterTimestamp;
+
+  final DateTime gpsTimestamp;
+  final GpsData  gps;
+
+  final DateTime rawAccelTimestamp;
+  final Vector3D rawAccel;
+
+  final DateTime cleanAccelTimestamp;
+  final Vector3D cleanAccel;
+
+  final DateTime gyroTimestamp;
+  final Vector3D gyro;
+
+  AggregateSensorData({
+    required this.masterTimestamp,
+    required this.gpsTimestamp,
+    required this.gps,
+    required this.rawAccelTimestamp,
+    required this.rawAccel,
+    required this.cleanAccelTimestamp,
+    required this.cleanAccel,
+    required this.gyroTimestamp,
+    required this.gyro,
+  });
+}
--- lib/core/sensor_service.dart
+++ lib/core/sensor_service.dart
@@ -1,1 +1,14 @@
+import 'dart:async';
+import 'package:rxdart/rxdart.dart';
+import 'package:sensors_plus/sensors_plus.dart';
+import 'package:geolocator/geolocator.dart';
 
+class SensorService {
+  static final SensorService _instance = SensorService._internal();
+
+  bool isReady = false;
+
+  factory SensorService() => _instance;
+
+  SensorService._internal();
+}

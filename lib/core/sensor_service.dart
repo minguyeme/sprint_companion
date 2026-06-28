89670c55bab01c5de76cbb0209038a18bd25233d
Nguyễn Hoàng Minh <nhoangminh131@gmail.com>
2026-06-28 23:09:51 +0700

    feat: add sensor service -Streams aggregate sensor data (speed calculated from doppler shift, raw and clean accel, and gyro) at high frequency -Handles basic exceptions

--- lib/core/sensor_service.dart
+++ lib/core/sensor_service.dart
@@ -2,11 +2,13 @@ import 'dart:async';
 import 'package:rxdart/rxdart.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:sprint_companion/core/sensor_exception.dart';
 import 'package:sprint_companion/core/aggregate_sensor_data.dart';
 
 class SensorService {
   static final SensorService _instance = SensorService._internal();
 
+  bool _isInitialising = false;
   final _sensorOutputController = BehaviorSubject<AggregateSensorData>();
   StreamSubscription<AggregateSensorData>? _sensorSubscription;
 
@@ -16,81 +18,95 @@ class SensorService {
   Stream<AggregateSensorData> get sensorStream =>
       _sensorOutputController.stream;
 
-  void initialiseSensor() async {
-    if (_sensorSubscription != null) return;
+  Future<void> initialiseSensor() async {
+    try {
+      if (_sensorSubscription != null || _isInitialising == true) {
+        throw ServiceAlreadyActiveException();
+      }
+      _isInitialising = true;
 
-    if (!(await Geolocator.isLocationServiceEnabled())) return;
+      if (!(await Geolocator.isLocationServiceEnabled())) {
+        throw GpsDisabledException();
+      }
 
-    if (await Geolocator.checkPermission() == LocationPermission.denied) {
-      if (await Geolocator.requestPermission() != LocationPermission.whileInUse) return;
+      if (await Geolocator.checkPermission() == LocationPermission.denied) {
+        if (await Geolocator.requestPermission() !=
+            LocationPermission.whileInUse) {
+          throw GpsDeniedException();
+        }
+      }
+
+      final Stream<AccelerometerEvent> rawAccelChannel =
+          accelerometerEventStream();
+      final Stream<UserAccelerometerEvent> cleanAccelChannel =
+          userAccelerometerEventStream();
+      final Stream<GyroscopeEvent> gyroChannel = gyroscopeEventStream();
+      final Stream<Position> gpsChannel = Geolocator.getPositionStream(
+        locationSettings: AndroidSettings(
+          accuracy: LocationAccuracy.best,
+          distanceFilter: 0,
+          intervalDuration: const Duration(seconds: 1),
+        ),
+      );
+
+      final Stream<TimestampedVector3D> timestampedRawAccel = rawAccelChannel
+          .map(
+            (event) => (
+              timestamp: event.timestamp.millisecondsSinceEpoch,
+              x: event.x,
+              y: event.y,
+              z: event.z,
+            ),
+          );
+      final Stream<TimestampedVector3D> timestampedCleanAccel =
+          cleanAccelChannel.map(
+            (event) => (
+              timestamp: event.timestamp.millisecondsSinceEpoch,
+              x: event.x,
+              y: event.y,
+              z: event.z,
+            ),
+          );
+      final Stream<TimestampedVector3D> timestampedGyro = gyroChannel.map(
+        (event) => (
+          timestamp: event.timestamp.millisecondsSinceEpoch,
+          x: event.x,
+          y: event.y,
+          z: event.z,
+        ),
+      );
+      final Stream<TimestampedGpsSpeed> timestampedSpeed = gpsChannel.map(
+        (position) => (
+          timestamp: position.timestamp.millisecondsSinceEpoch,
+          speed: position.speed,
+          accuracy: position.speedAccuracy,
+        ),
+      );
+
+      _sensorSubscription =
+          CombineLatestStream.combine4(
+            timestampedRawAccel,
+            timestampedCleanAccel,
+            timestampedGyro,
+            timestampedSpeed,
+            (raw, clean, gyro, gps) => AggregateSensorData(
+              rawAccel: raw,
+              cleanAccel: clean,
+              gyro: gyro,
+              gps: gps,
+            ),
+          ).listen(
+            (aggregateData) {
+              _sensorOutputController.add(aggregateData);
+            },
+            onError: (error, stackTrace) {
+              _sensorOutputController.addError(error, stackTrace);
+            },
+          );
+    } catch (exception, stackTrace) {
+      _isInitialising = false;
+      _sensorOutputController.addError(exception, stackTrace);
     }
-
-    final Stream<AccelerometerEvent> rawAccelChannel =
-        accelerometerEventStream();
-    final Stream<UserAccelerometerEvent> cleanAccelChannel =
-        userAccelerometerEventStream();
-    final Stream<GyroscopeEvent> gyroChannel = gyroscopeEventStream();
-    final Stream<Position> gpsChannel = Geolocator.getPositionStream(
-      locationSettings: AndroidSettings(
-        accuracy: LocationAccuracy.best,
-        distanceFilter: 0,
-        intervalDuration: const Duration(seconds: 1),
-      ),
-    );
-
-    final Stream<TimestampedVector3D> timestampedRawAccel = rawAccelChannel.map(
-      (event) => (
-        timestamp: event.timestamp.millisecondsSinceEpoch,
-        x: event.x,
-        y: event.y,
-        z: event.z,
-      ),
-    );
-    final Stream<TimestampedVector3D> timestampedCleanAccel = cleanAccelChannel
-        .map(
-          (event) => (
-            timestamp: event.timestamp.millisecondsSinceEpoch,
-            x: event.x,
-            y: event.y,
-            z: event.z,
-          ),
-        );
-    final Stream<TimestampedVector3D> timestampedGyro = gyroChannel.map(
-      (event) => (
-        timestamp: event.timestamp.millisecondsSinceEpoch,
-        x: event.x,
-        y: event.y,
-        z: event.z,
-      ),
-    );
-    final Stream<TimestampedGpsSpeed> timestampedSpeed = gpsChannel.map(
-      (position) => (
-        timestamp: position.timestamp.millisecondsSinceEpoch,
-        speed: position.speed,
-        accuracy: position.speedAccuracy,
-      ),
-    );
-
-    _sensorSubscription =
-        CombineLatestStream.combine4(
-          timestampedRawAccel,
-          timestampedCleanAccel,
-          timestampedGyro,
-          timestampedSpeed,
-          (raw, clean, gyro, gps) => AggregateSensorData(
-            rawAccel: raw,
-            cleanAccel: clean,
-            gyro: gyro,
-            gps: gps,
-          ),
-        ).listen(
-          (aggregateData) {
-            _sensorOutputController.add(aggregateData);
-          },
-          onError: (error, stackTrace) {
-            _sensorOutputController.addError(error, stackTrace);
-          },
-        );
   }
 
   void dispose() {

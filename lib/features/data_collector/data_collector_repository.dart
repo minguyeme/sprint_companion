51b7d533fb56e87ee2051bc7c3a719fe56df9685
Nguyễn Hoàng Minh <nhoangminh131@gmail.com>
2026-07-02 03:38:13 +0700

    wip(DataCollectorRepository): add stop recording and save cache functionality

--- lib/features/data_collector/data_collector_repository.dart
+++ lib/features/data_collector/data_collector_repository.dart
@@ -1,11 +1,24 @@
 import 'dart:async';
+import 'package:flutter/foundation.dart';
+import 'package:csv/csv.dart';
 import '../../core/aggregate_sensor_data.dart';
 import '../../core/sensor_service.dart';
+import '../../core/file_service.dart';
 
-enum CollectorError { activeService, activeRecording, notRecording, gpsDisabled, gpsDenied, unknown }
+enum CollectorError {
+  activeService,
+  activeRecording,
+  notRecording,
+  gpsDisabled,
+  gpsDenied,
+  outOfStorage,
+  sensorUnknown,
+  fileUnknown,
+}
 
 class DataCollectorRepository {
   final _sensorService = SensorService();
+  final _fileService = FileService();
   final List<List<num>> _sensorCache = [];
   StreamSubscription<AggregateSensorData>? _sensorStream;
 
@@ -20,7 +33,7 @@ class DataCollectorRepository {
           ServiceAlreadyActiveException() => CollectorError.activeService,
           GpsDisabledException() => CollectorError.gpsDisabled,
           GpsDeniedException() => CollectorError.gpsDenied,
-          _ => CollectorError.unknown,
+          _ => CollectorError.sensorUnknown,
         },
       );
     }
@@ -57,12 +70,67 @@ class DataCollectorRepository {
       onError: (error) => switch (error) {
         GpsDeniedException() => onError(CollectorError.gpsDenied),
         GpsDisabledException() => onError(CollectorError.gpsDisabled),
-        _ => onError(CollectorError.unknown),
+        _ => onError(CollectorError.sensorUnknown),
       },
     );
   }
 
-  void stopRecording({required void Function(CollectorError) onError}) {
-    if (_sensorStream == null) onError(CollectorError.notRecording);
+  Future<void> stopRecording({
+    required void Function(CollectorError) onError,
+  }) async {
+    if (_sensorStream == null || _sensorCache.isEmpty) {
+      onError(CollectorError.notRecording);
+      return;
+    }
+    await _sensorStream?.cancel();
+    _sensorStream = null;
   }
+
+  Future<void> saveCache({
+    required void Function(CollectorError) onError,
+    required void Function() onCompletion,
+  }) async {
+    try {
+      if (_sensorStream != null) onError(CollectorError.activeRecording);
+      String csvString = await compute(_toCsvWorker, _sensorCache);
+      await _fileService.saveUserData(
+        csvString,
+        fileName: '',
+        type: FileType.dataset,
+      );
+      onCompletion();
+      _sensorCache.clear();
+    } catch (exception) {
+      switch (exception) {
+        case OutOfStorageException():
+          onError(CollectorError.outOfStorage);
+        case UnknownStorageException():
+          onError(CollectorError.fileUnknown);
+      }
+    }
+  }
+}
+
+Future<String> _toCsvWorker(List<List<num>> matrix) async {
+  final List<List<dynamic>> csvMatrix = [
+    [
+      'raw_accel_timestamp',
+      'raw_accel_x',
+      'raw_accel_y',
+      'raw_accel_z',
+      'clean_accel_timestamp',
+      'clean_accel_x',
+      'clean_accel_y',
+      'clean_accel_z',
+      'gyro_timestamp',
+      'gyro_x',
+      'gyro_y',
+      'gyro_z',
+      'gps_timestamp',
+      'speed',
+      'speed_accuracy',
+    ],
+    ...matrix,
+  ];
+  return csv.encode(csvMatrix);
 }

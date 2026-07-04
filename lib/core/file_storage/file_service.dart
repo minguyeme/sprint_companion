9a9770f92c8b433c53155ebb19873011c5ae3208
Nguyễn Hoàng Minh <nhoangminh131@gmail.com>
2026-07-04 19:56:38 +0700

    feat(FileService): add file exploration capability

--- lib/core/file_storage/file_service.dart
+++ lib/core/file_storage/file_service.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'dart:developer' as developer;
 import 'package:path_provider/path_provider.dart';
+import 'package:sprint_companion/core/file_storage/managed_file_data.dart';
 
 sealed class FileException implements Exception {}
 
@@ -51,11 +52,50 @@ class FileService {
     await _writeStringWorker(targetPath, str);
   }
 
+  Future<List<ManagedFileData>> getUserDataFiles({
+    required FileType type,
+  }) async {
+    _documentsPath ??= (await getApplicationDocumentsDirectory()).path;
+    final targetPath = '$_documentsPath/${type.parentPath}';
+
+    return await _getFilesWorker(targetPath);
+  }
+
+  Future<List<ManagedFileData>> _getFilesWorker(String path) async {
+    try {
+      final targetDir = Directory(path);
+      await targetDir.create(recursive: true);
+      final files = (await targetDir.list().toList()).whereType<File>();
+      return await Future.wait(
+        files.map(
+          (file) async => ManagedFileData(
+            path: file.path,
+            name: file.path.split('/').last,
+            sizeInBytes: await file.length(),
+          ),
+        ),
+      );
+      
+    } catch (exception, stackTrace) {
+      developer.log(
+        'Failure to get files.',
+        name: 'FileService',
+        error: exception,
+        stackTrace: stackTrace,
+      );
+      switch (exception) {
+        default:
+          throw UnknownStorageException();
+      }
+    }
+  }
+
   Future<void> _writeStringWorker(String path, String content) async {
     try {
       final targetFile = File(path);
       await targetFile.create(recursive: true);
       await targetFile.writeAsString(content);
+
     } on FileSystemException catch (fileSystemException, stackTrace) {
       developer.log(
         'File system failure.',
---
+++ lib/core/file_storage/managed_file_data.dart
@@ -0,0 +1,11 @@
+class ManagedFileData {
+  final String path;
+  final String name;
+  final int sizeInBytes;
+
+  ManagedFileData({
+    required this.path,
+    required this.name,
+    required this.sizeInBytes
+  });
+}

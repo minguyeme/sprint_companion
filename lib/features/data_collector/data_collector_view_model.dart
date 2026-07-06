5d4080e60c225e04ab799603edda998587f7f31d
Nguyễn Hoàng Minh <nhoangminh131@gmail.com>
2026-07-06 13:19:56 +0700

    feat(DataCollectorViewModel): add working viewmodel for the data collector page

--- lib/features/data_collector/data_collector_view_model.dart
+++ lib/features/data_collector/data_collector_view_model.dart
@@ -1,20 +1,23 @@
 import 'dart:async';
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:sprint_companion/core/file_storage/file_service.dart';
 import 'package:sprint_companion/core/file_storage/managed_file_data.dart';
 import 'data_collector_repository.dart';
 import '../../core/file_storage/file_management_repository.dart';
 
 class DataCollectorViewModel extends ChangeNotifier {
+  final nameController = TextEditingController();
   final FileManagementRepository _fileRepository;
   final DataCollectorRepository _collectorRepository;
 
   StreamSubscription<FileManagementStatus>? _fileStatusSubscription;
+  StreamSubscription<List<ManagedFileData>>? _managedFilesSubscription;
   StreamSubscription<CollectorStatus>? _collectorStatusSubscription;
+  var _cachedInfo = (rows: 0, maxSpeed: 0.0, maxSpeedAccuracy: 0.0);
 
   FileManagementStatus _fileStatus = FileManagementStatus.inactive;
   CollectorStatus _collectorStatus = CollectorStatus.inactive;
-  List<ManagedFileData> _managedFiles = [];
+  List<ManagedFileData> _savedDatasets = [];
   SessionFlag _selectedFlag = SessionFlag.sprint;
 
   DataCollectorViewModel({
@@ -25,27 +28,55 @@ class DataCollectorViewModel extends ChangeNotifier {
       _fileStatus = status;
       notifyListeners();
     });
+
+    _managedFilesSubscription = _fileRepository.managedFilesStream.listen((
+      list,
+    ) {
+      _savedDatasets = list;
+      notifyListeners();
+    });
+
     _collectorStatusSubscription = _collectorRepository.statusStream.listen((
       status,
     ) {
       _collectorStatus = status;
+      switch (_collectorStatus) {
+        case CollectorStatus.idle:
+          nameController.clear();
+          _cachedInfo = (rows: 0, maxSpeed: 0.0, maxSpeedAccuracy: 0.0);
+        case CollectorStatus.cached:
+          _cachedInfo = _collectorRepository.cacheInfo();
+        default:
+      }
       notifyListeners();
     });
   }
 
   FileManagementStatus get fileStatus => _fileStatus;
   CollectorStatus get collectorStatus => _collectorStatus;
-  List<ManagedFileData> get managedFiles => _managedFiles;
+  List<ManagedFileData> get savedDatasets => _savedDatasets;
+
   SessionFlag get selectedFlag => _selectedFlag;
+  set selectedFlag(SessionFlag flag) {
+    _selectedFlag = flag;
+    notifyListeners();
+  }
+
+  ({String rows, String maxSpeed, String maxSpeedAccuracy}) get cacheInfo => (
+    rows: '${_cachedInfo.rows} rows',
+    maxSpeed: '${_cachedInfo.maxSpeed.toStringAsFixed(2)} m/s',
+    maxSpeedAccuracy:
+        '± ${_cachedInfo.maxSpeedAccuracy.toStringAsFixed(2)} m/s',
+  );
 
   void initialiseScreen({
     required void Function(CollectorError) uiOnCollectorError,
     required void Function(FileManagementError) uiOnFileError,
   }) {
-    if (_fileStatus != FileManagementStatus.inactive) {
+    if (_fileStatus == FileManagementStatus.inactive) {
       _fileRepository.initialiseFor(FileType.dataset, onError: uiOnFileError);
     }
-    if (_collectorStatus != CollectorStatus.inactive) {
+    if (_collectorStatus == CollectorStatus.inactive) {
       _collectorRepository.initialiseSession(onError: uiOnCollectorError);
     }
   }
@@ -60,7 +91,7 @@ class DataCollectorViewModel extends ChangeNotifier {
         _collectorRepository.stopRecording(onError: uiOnError);
       case CollectorStatus.cached:
         _collectorRepository.saveCache(
-          name: 'name',
+          name: nameController.text.trim().isEmpty ? null : nameController.text,
           flag: _selectedFlag,
           onError: uiOnError,
         );
@@ -68,9 +99,25 @@ class DataCollectorViewModel extends ChangeNotifier {
     }
   }
 
+  void handleDelete(
+    ManagedFileData file, {
+    required void Function(FileManagementError) uiOnError,
+  }) {
+    _fileRepository.delete(file, onError: uiOnError);
+  }
+
+  void handleShare(
+    ManagedFileData file, {
+    required void Function(bool) uiOnResult,
+  }) {
+    _fileRepository.share(file, onResult: uiOnResult);
+  }
+
   @override
   void dispose() {
+    nameController.dispose();
     _fileStatusSubscription?.cancel();
+    _managedFilesSubscription?.cancel();
     _collectorStatusSubscription?.cancel();
     super.dispose();
   }

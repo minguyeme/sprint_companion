9b875b8d026f8f35c322bf072ba4f2400a9a431a
Nguyễn Hoàng Minh <nhoangminh131@gmail.com>
2026-07-14 05:31:56 +0700

    refactor(DataCollectorViewModel)!: switch from delegating error display to the view to maintaining a status info message.

--- lib/features/data_collector/data_collector_view_model.dart
+++ lib/features/data_collector/data_collector_view_model.dart
@@ -1,9 +1,9 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:sprint_companion/core/file_storage/file_service.dart';
-import 'package:sprint_companion/core/file_storage/managed_file_data.dart';
-import 'data_collector_repository.dart';
+import '../../core/file_storage/file_service.dart';
+import '../../core/file_storage/managed_file_data.dart';
 import '../../core/file_storage/file_management_repository.dart';
+import 'data_collector_repository.dart';
 
 class DataCollectorViewModel extends ChangeNotifier {
   final nameController = TextEditingController();
@@ -13,6 +13,7 @@ class DataCollectorViewModel extends ChangeNotifier {
   StreamSubscription<FileManagementStatus>? _fileStatusSubscription;
   StreamSubscription<List<ManagedFileData>>? _managedFilesSubscription;
   StreamSubscription<CollectorStatus>? _collectorStatusSubscription;
+  String _statusMessage = 'Awaiting sensors';
 
   FileManagementStatus _fileStatus = FileManagementStatus.inactive;
   CollectorStatus _collectorStatus = CollectorStatus.inactive;
@@ -54,6 +55,7 @@ class DataCollectorViewModel extends ChangeNotifier {
   FileManagementStatus get fileStatus => _fileStatus;
   CollectorStatus get collectorStatus => _collectorStatus;
   List<ManagedFileData> get savedDatasets => _savedDatasets;
+  String get statusMessage => _statusMessage;
 
   ({String rows, String maxSpeed, String maxSpeedAccuracy}) get cacheInfo => (
     rows: '${_cachedInfo.rows} rows',
@@ -62,56 +64,102 @@ class DataCollectorViewModel extends ChangeNotifier {
         '± ${_cachedInfo.maxSpeedAccuracy.toStringAsFixed(2)} m/s',
   );
 
-  void initialiseScreen({
-    required void Function(CollectorError) uiOnCollectorError,
-    required void Function(FileManagementError) uiOnFileError,
-  }) {
+  void initialiseScreen() {
     if (_fileStatus == FileManagementStatus.inactive) {
-      _fileRepository.initialiseFor(FileType.dataset, onError: uiOnFileError);
+      _fileRepository.initialiseFor(
+        FileType.dataset,
+        onError: _handleFileError,
+      );
     }
     if (_collectorStatus == CollectorStatus.inactive) {
-      _collectorRepository.initialiseSession(onError: uiOnCollectorError);
+      _collectorRepository.initialiseSession(onError: _handleCollectorError);
     }
   }
 
-  void handlePrimaryButtonPress({
-    required void Function(CollectorError) uiOnError,
-  }) {
+  void handlePrimaryButtonPress() {
     switch (_collectorStatus) {
       case CollectorStatus.idle:
-        _collectorRepository.startRecording(onError: uiOnError);
+        _collectorRepository.startRecording(onError: _handleCollectorError);
       case CollectorStatus.recording:
-        _collectorRepository.stopRecording(onError: uiOnError);
+        _collectorRepository.stopRecording(onError: _handleCollectorError);
       case CollectorStatus.cached:
         final name = nameController.text.trim();
         _collectorRepository.saveCache(
           name: name.isEmpty ? null : name,
-          onError: uiOnError,
+          onError: _handleCollectorError,
         );
       default:
     }
   }
 
-  void handleDelete(
-    ManagedFileData file, {
-    required void Function(FileManagementError) uiOnError,
-  }) {
-    _fileRepository.delete(file, onError: uiOnError);
+  void handleDelete(ManagedFileData file) {
+    _fileRepository.delete(file, onError: _handleFileError);
   }
 
-  void handleRename(
-    ManagedFileData file, {
-    required String name,
-    required void Function(FileManagementError) uiOnError,
-  }) {
-    _fileRepository.rename(file, newName: name, onError: uiOnError);
+  void handleRename(ManagedFileData file, {required String name}) {
+    _fileRepository.rename(file, newName: name, onError: _handleFileError);
   }
 
-  void handleShare(
-    ManagedFileData file, {
-    required void Function(bool) uiOnResult,
-  }) {
-    _fileRepository.share(file, onResult: uiOnResult);
+  void handleShare(ManagedFileData file) {
+    _fileRepository.share(
+      file,
+      onResult: (bool isSucessful) {
+        if (isSucessful) {
+          _statusMessage = 'File shared successfully.';
+        } else {
+          _statusMessage = 'Failed to share file.';
+        }
+        notifyListeners();
+      },
+    );
+  }
+
+  void _handleFileError(FileManagementError error) {
+    _statusMessage = switch (error) {
+      FileManagementError.alreadyActive =>
+        'A file management instance is already running.',
+      FileManagementError.unknownFile =>
+        'Could not read or write data. Please check your storage availability.',
+    };
+    notifyListeners();
+  }
+
+  void _handleCollectorError(CollectorError error) {
+    _statusMessage = switch (error) {
+      CollectorError.inactiveService =>
+        'Collector is uninitialised. Please initialise or restart the page',
+
+      CollectorError.activeService =>
+        'A collection session is already running.',
+
+      CollectorError.activeRecording =>
+        'Cannot perform action while recording is actively running.',
+
+      CollectorError.noRecording =>
+        'No active recording session was found to stop or save.',
+
+      CollectorError.gpsDisabled =>
+        'GPS is turned off. Please enable location services.',
+
+      CollectorError.gpsDenied =>
+        'Location permission denied. The app needs gps to function.',
+
+      CollectorError.gpsPermaDenied =>
+        'Location access is blocked. Please enable it in your device\'s settings',
+
+      CollectorError.preciseGpsDenied =>
+        'Precise location is required. Please allow precise location.',
+
+      CollectorError.outOfStorage =>
+        'Storage is full. Please clear some space on your device.',
+
+      CollectorError.sensorUnknown =>
+        'Failed to connect to tracking sensors. Please restart the app.',
+
+      CollectorError.fileUnknown =>
+        'Could not read or write data. Please check your storage availability.',
+    };
+    notifyListeners();
   }
 
   @override
